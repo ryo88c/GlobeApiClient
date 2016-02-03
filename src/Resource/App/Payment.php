c2f85e8810f601ec84bd1939a3dbc85b32908da5
Ryo HAYASHI <ryo88c@gmail.com>
2016-02-03 12:09:24 +0000

    Payment API 用リソースの実装

---
+++ src/Resource/App/Payment.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the Ryo88c\GlobeApiClient package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ryo88c\GlobeApiClient\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+use Ryo88c\GlobeApiClient\Inject\GlobeApiClientInject;
+
+class Payment extends ResourceObject
+{
+    use GlobeApiClientInject;
+
+    private $version = 'v1';
+
+    /**
+     * /queries/location
+     *
+     * @see https://docs.google.com/document/d/1UUut5jbmdatDOi9I1znKSSHKywPWZ1oXussg7AlnXik/pub?embedded=true
+     *
+     * @return $this
+     */
+    public function onPost()
+    {
+        $this['result'] = $this->request('/transactions/amount');
+        return $this;
+    }
+}
